a15c3cab36392e3236d2864acc544b9d25d4d27d
apantol <you@example.com>
2017-10-16 20:04:46 +0200

    Fixed byte operations...

--- tempSensor.py
+++ tempSensor.py
@@ -9,9 +9,9 @@ class TempSensor():
 
 	def readTemp():
 		print('Reading temperature value:  ')
-		self.i2cBus.write_byte(self.tempSensAddr, self.tempSensAddr && 0xFE)
+		self.i2cBus.write_byte(self.tempSensAddr, self.tempSensAddr & 0xFE)
 		self.i2cBus.write_byte(self.tempSensAddr, 0x05)
-		self.i2cBus.write_byte(self.tempSensAddr, self.tempSensAddr && 0x01)
+		self.i2cBus.write_byte(self.tempSensAddr, self.tempSensAddr & 0x01)
 		pass
 
 	def decodeTemp(upperByte, lowerByte):
